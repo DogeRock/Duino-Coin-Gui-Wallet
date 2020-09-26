d2e691387fef210d08bc62813c57a08c33847e4e
DogeRock <71775028+DogeRock@users.noreply.github.com>
2020-09-25 21:59:25 -0400

    Update Wallet.py

--- Wallet.py
+++ Wallet.py
@@ -3,7 +3,7 @@
 from tkinter import * 
 from tkinter import messagebox
 from tkinter import ttk
-import socket, os, urllib.request
+import socket, os, urllib.request, time, requests, json
 
 try: 
   import popup
@@ -26,13 +26,31 @@ def popupmsg(msg, title):\n
 """)
     f.close()
 
-import popup
-
 top = Tk()
 
 top.configure(background="#7d796e")
-top.geometry("500x500")
+top.geometry("700x700")
 top.title("DogeRock Duino-Coin Gui Wallet")
+
+current_hour = time.strptime(time.ctime(time.time())).tm_hour
+if current_hour < 12:
+  L5 = Label(top, text = "Good morning!")
+  L5.place(x = 200, y = 100)
+elif current_hour == 12:
+  L6 = Label(top, text = "Mmmm lunch time")
+  L6.place(x = 200, y = 100)
+elif current_hour > 12 and current_hour < 18:
+  L7 = Label(top, text = "Good afternoon")
+  L7.place(x = 200, y = 100)
+elif current_hour >= 18 and current_hour < 23:
+  L8 = Label(top, text = "Good evening")
+  L8.place(x = 200, y = 100)
+elif current_hour >= 23:
+  L9 = Label(top, text = "Yawn")
+  L9.place(x = 200, y = 100)
+else:
+  L10 = Label(top, text = "Welcome back")
+  L10.place(x = 200, y = 10)
 
 L1 = Label(top, text = "User Name")
 L1.place(x = 1,y = 200)
@@ -75,5 +93,27 @@ def GetBalance():
   L4 = Label(top, text = balance)
   L4.place(x = 200, y = 350)
 
+  api = "https://raw.githubusercontent.com/revoxhere/duco-statistics/master/api.json"
+
+  apiFeedback = requests.get(api, data = None)
+
+  rate = 1
+
+  if apiFeedback.status_code == 200:
+    content = apiFeedback.content.decode()
+    contentjson = json.loads(content)
+    ducofiat = float(contentjson["Duco price"]) * float(rate)
+    L11 = Label(top, text = "Duino price:")
+    L12 = Label(top, text = ducofiat)
+    L11.place(x = 200, y = 400)
+    L12.place(x = 200, y = 425)
+    DuinoFiatValue = float(balance) * float(ducofiat)
+    L13 = Label(top, text = "Fiat value:")
+    L14 = Label(top, text = DuinoFiatValue)
+    L13.place(x = 200, y = 475)
+    L14.place(x = 200, y = 500)
+  else:
+    popup.popupmsg("Error: Cannot send request to api, Please try again later", "Error")
+
 B = Button(top, text = "Check balance", command=GetBalance)
-B.place(x = 200, y = 400)
+B.place(x = 200, y = 550)
